f1db4e97a0d08422b4b3b42c3b9c7ec2c0ad0d7f
Rebecca Weiss <rebecca.j.weiss@gmail.com>
2015-11-18 17:52:20 -0800

    Removed some commented out code, refactored some output code to create a diagnostic report.

--- page_processor.py
+++ page_processor.py
@@ -114,21 +114,24 @@ class AlamedaPageProcessor(PageProcessor):
 
 		self.logger.info('alameda parse complete, {x} page(s) failed'.format(x=self._num_failed))
 		self.create_files()
+		with codecs.open('page_processor_meta.txt', 'a', 'utf8') as outfile:
+			outfile.write('alameda,{rate}\n'.format(rate=float(self._num_failed)/self._totalpages))
 
 	def get_data(self, page):
 		data = split(page.text, ['Dem', 'Rep'])
 		self.logger.info('{num} lines found on page {id} and roll {rollnum}'.format(num=len(data), id=page.id, rollnum=page.rollnum))
-		#return "{id},{rollnum},{date},{data}".format(id=page.id, rollnum=page.rollnum, date=page.date, data=data)
 		return {'id':page.id, 'rollnum':page.rollnum, 'date':page.date, 'data':data}
 
 	def create_files(self):
 		# create good data file
+		self.logger.info('writing alameda successes to file')
 		with codecs.open('alameda_successes.txt', 'a', 'utf8') as outfile:
 				for page in self._validdata:
 					for line in page['data']:
 						outfile.write("{id},{rollnum},{date},{data}\n".format(id=page['id'], rollnum=page['rollnum'], date=page['date'], data=line))
-#						outfile.write("%s\n" % line)
+
 		# create bad data file
+		self.logger.info('writing alameda failures to file')
 		if self._num_failed > 0:
 			with codecs.open('alameda_failures.txt', 'a', 'utf8') as outfile:
 				for line in self._failures:
@@ -140,7 +143,6 @@ class AlamedaPage(Page):
 		Page.__init__(self, rawtext)
 
 	def parse_file(self):
-		#self.logger.info('converting line to page')
 		lines = self.rawtext.split(u'|')
 		try:
 			self.set_id(lines[1])
@@ -150,17 +152,15 @@ class AlamedaPage(Page):
 			self.set_rollnum(lines[3].split(' ')[1])
 		except:
 			self.logger.warn('Failed to get page roll number')
-		self._text = lines[5]
+		self._text = ''.join(lines[4:])
 		self.set_date(self._get_date(self.text))
 		if self.date and self.id:
 			return
-		#else:
-			#self.logger.error(text)
 
 
 	def _get_date(self, text):
-		datepattern = r'\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)\s(\d{0,2})\s(\d{0,4})'
-		match = re.match(datepattern, text)
+		datepattern = r'(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)\s(\d{0,2})\s(\d{0,4})'
+		match = re.search(datepattern, text)
 		if match:
 			return match.group(0)
 		else:
@@ -168,7 +168,6 @@ class AlamedaPage(Page):
 
 	def has_valid_data(self):
 		if self.id and self.rollnum and self.text and self.date:
-		#if self.date:
 			return True
 		else:
 			return False
